c84663791c7dff8f2453c207b627e22174ccc00e
Max Hentschel <max.hentschel@e-t-a.de>
2024-12-03 08:29:04 +0100

    [03]: completed

---
+++ aoc_03_mul_instructions/src/Start.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Start {
+    private String input;
+    private ArrayList<String> mulExpressions;
+    private int result;
+    public Start(){
+        mulExpressions = new ArrayList<>();
+        result = 0;
+        readInput();
+        separateMulExpressions();
+        calculateResult();
+        System.out.println("result = " + result);
+    }
+
+    public static void main(String[] args) {
+        new Start();
+    }
+
+    private void readInput(){
+        Path filePath = Paths.get("./input.txt");
+        try {
+            this.input = Files.readString(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void separateMulExpressions(){
+        //String regex = "mul\\(([1-9][0-9]{0,2}),([1-9][0-9]{0,2})\\)";
+        String regex = "(mul\\(([1-9][0-9]{0,2}),([1-9][0-9]{0,2})\\))|(do\\(\\))|(don't\\(\\))";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find()){
+            mulExpressions.add(matcher.group());
+        }
+    }
+
+    private void calculateResult(){
+        String regex = "[1-9][0-9]{0,2}";
+        Pattern pattern = Pattern.compile(regex);
+        ArrayList<Integer> factors = new ArrayList<>();
+        boolean validMul = true;
+
+        for(String mul : mulExpressions){
+            if(Objects.equals(mul, "don't()")){
+                validMul = false;
+            }else if(Objects.equals(mul, "do()")){
+                validMul = true;
+            }
+            if(validMul && !Objects.equals(mul, "do()")){
+                Matcher matcher = pattern.matcher(mul);
+                while(matcher.find()){
+                    factors.add(Integer.parseInt(matcher.group()));
+                }
+                result += (factors.getFirst() * factors.getLast());
+                factors.clear();
+            }
+        }
+    }
+}
